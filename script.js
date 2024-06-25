6e2ea8a0ffa76e5d4470009ed4d03cff20930318
ElaLestari <149997548+ElaLestari@users.noreply.github.com>
2024-06-25 19:36:49 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function changeContent() {
+    var element = document.getElementById("content");
+    element.innerHTML = "wwaahhhh";
+    document.getElementById("awwch").src = "https://i.pinimg.com/564x/29/d1/b7/29d1b7fbcd46c05bfc216d72ee853d41.jpg" ;
+}
